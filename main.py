9c49f5515038b0f7cbccc68deab87a199b6f46a7
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-09-13 16:22:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,30 @@ import streamlit as st
 from apikey import apikey
 
 def generate_image(image_description):
-  client = OpenAI()
-  response = client.images.generate(
-  model="dall-e-3",
-  prompt=f"{image_description}",
-  size="1024x1024",
-  quality="standard",
-  n=1,
-  )
+    client = OpenAI(api_key=apikey)
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=f"{image_description}",
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
 
-  img_url = response.data[0].url
-  return img_url
+    img_url = response.data[0].url
+    return img_url
 
-st.set_page_config(page_title="DALL.E 3 Image Generation")
+st.set_page_config(page_title="DALL-E 3 Image Generation")
 
-openai.api_key = apikey
-
-st.title('DALL.E 3 Image Generation')
+st.title('DALL-E 3 Image Generation')
 st.subheader("Powered by OpenAI and Streamlit")
-img_description = st.text_input('Image Desription')
+img_description = st.text_input('Image Description')
 
 if st.button('Generate Image'):
-    generated_img = generate_image(img_description)
-    st.image(generated_img)
+    if img_description:
+        try:
+            generated_img = generate_image(img_description)
+            st.image(generated_img)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+    else:
+        st.warning("Please enter an image description.")
